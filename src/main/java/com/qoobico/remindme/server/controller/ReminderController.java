7e8edfd9458a8f1907833ec2f5869e369f2bd592
AleksandrGr <kharohiy@gmail.com>
2017-12-15 11:56:13 +0200

    add config

---
+++ src/main/java/com/qoobico/remindme/server/controller/ReminderController.java
@@ -0,0 +1,16 @@
+package com.qoobico.remindme.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/remind")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public String getReminder() {
+        return "My reminder";
+    }
+
+}
